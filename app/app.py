72227f11547f3c00d6c05b5029457e7132547326
Khushi Patel <patelkhushi7559@gmail.com>
2026-04-23 13:40:51 +0530

    added requirements

--- app/app.py
+++ app/app.py
@@ -1,34 +1,33 @@
 import streamlit as st
-import numpy as np
-from tensorflow.keras.models import load_model
-
-st.set_page_config(page_title="Anomaly Detection", layout="centered")
+import requests
 
 st.title("🔥 Industrial Fault Detection System")
 
-st.write("Enter temperature values to check anomaly:")
-
-# Load model
-model = load_model("models/autoencoder_model.keras")
-
-THRESHOLD = 0.01
-
-# Inputs
 center_temp = st.number_input("Center Temperature", value=0.0)
 max_temp = st.number_input("Max Temperature", value=0.0)
 min_temp = st.number_input("Min Temperature", value=0.0)
 
-# Button
 if st.button("Predict"):
-    input_data = np.array([[center_temp, max_temp, min_temp]])
+    data = {
+        "center_temp": center_temp,
+        "max_temp": max_temp,
+        "min_temp": min_temp
+    }
 
-    reconstructed = model.predict(input_data)
-    error = np.mean((input_data - reconstructed) ** 2)
+    try:
+        response = requests.post(
+            "http://127.0.0.1:8000/predict",
+            json=data
+        )
 
-    st.subheader("Result")
-    st.write(f"Reconstruction Error: {error}")
+        result = response.json()
 
-    if error > THRESHOLD:
-        st.error("⚠️ Anomaly Detected!")
-    else:
-        st.success("✅ Normal Data")
+        st.write(f"Reconstruction Error: {result['reconstruction_error']}")
+
+        if result["anomaly"]:
+            st.error("⚠️ Anomaly Detected!")
+        else:
+            st.success("✅ Normal Data")
+
+    except:
+        st.error("❌ API not running. Start FastAPI first.")
